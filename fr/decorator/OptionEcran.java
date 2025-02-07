7379c0dc642cee980edc273d72d6b5f510324a59
Beedy <beedywool@gmail.com>
2025-02-07 14:35:25 +0100

    Add documentation comments to OptionEcran class and its methods

--- fr/decorator/OptionEcran.java
+++ fr/decorator/OptionEcran.java
@@ -2,16 +2,32 @@ package fr.decorator;
 
 import fr.model.Appareil;
 
+/**
+ * Classe OptionEcran dans le decorateur pour ajouter l'option ecran à un appareil
+ */
 public class OptionEcran extends AppareilDecorateur {
 
+    /**
+     * Constructeur de la classe OptionEcran
+     * @param appareil Appareil
+     */
 	public OptionEcran(Appareil appareil) {
 		super(appareil);
 	}
 	
+    /**
+     * Méthode getDescription pour retourner la description de l'appareil
+     * @return Appareil + Ajout d'ecrans (250€)
+     */
     @Override
     public String getDescription() {
         return super.getDescription() + " + Ajout d'ecrans (250€)";
     }
+
+    /**
+     * Méthode getPrixFinale pour retourner le prix final de l'appareil
+     * @return Prix final de l'appareil + 250
+     */
     @Override
     public double getPrixFinale() {
         return super.getPrixFinale() + 250;
